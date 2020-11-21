909ebf9050edca5183e0f6bc2d47966aba61390e
Daniel Rueda <danruedalop@gmail.com>
2020-11-21 16:03:13 +0100

    finished

---
+++ practica1/src/Vec2.cpp
@@ -0,0 +1,99 @@
+
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include "Vec2.h"
+#include <math.h>
+
+
+
+Vec2::Vec2(float pos_x, float pos_y)
+{
+	this->posX = pos_x;
+	this->posY = pos_y;
+
+};
+
+
+//A + B
+Vec2 Vec2::sumVec2(const Vec2& other)
+{
+	float sX = Vec2::getPosX() + other.posX;
+	float sY = Vec2::getPosY() + other.posY;
+	return Vec2(sX, sY);
+}
+;
+
+//A - B
+Vec2 Vec2::diffVec2(const Vec2& other)
+{
+	float sX = Vec2::getPosX() - other.posX;
+	float sY = Vec2::getPosY() - other.posY;
+	return Vec2(sX, sY);
+}
+;
+
+// Vector * Scalar
+Vec2 Vec2::scaleVec2(const float num)
+{
+	float sX = Vec2::getPosX() * num;
+	float sY = Vec2::getPosY() * num;
+	return Vec2(sX, sY);
+}
+;
+
+// Vector / Scalar
+Vec2 Vec2::divVec2(const float num)
+{
+	float sX = Vec2::getPosX() / num;
+	float sY = Vec2::getPosY() / num;
+	return Vec2(sX, sY);
+}
+;
+
+// Dot Product / Scalar Product
+float Vec2::scalarProdVec2(const Vec2& other)
+{
+	return (Vec2::getPosX() * other.posX) + (Vec2::getPosY() * other.posY);
+};
+
+Vec2 Vec2::absVec2()
+{
+	float aX;
+	float aY;
+	//string result = (time < 18) ? "Good day." : "Good evening.";
+	aX = (Vec2::getPosX() < 0) ? -Vec2::getPosX() : Vec2::getPosX();
+	aY = (Vec2::getPosY() < 0) ? -Vec2::getPosY() : Vec2::getPosY();
+	return Vec2(aX, aY);
+};
+float Vec2::normVec2()
+{
+	double pow1 = pow(Vec2::getPosX(), 2);
+	double pow2 = pow(Vec2::getPosY(), 2);
+	float sol = sqrt(pow1 + pow2);
+
+	return sol;
+};
+
+float Vec2::lenVec2()
+{
+	return Vec2::getThisVector2D().normVec2();
+};
+
+float Vec2::angleVec2(const Vec2& other)
+{
+	float angle = atan2(other.posY, other.posX) - atan2(Vec2::getPosY(), Vec2::getPosX());
+	if (angle > M_PI) {
+		angle -= 2 * M_PI;
+	}
+	else if (angle <= -M_PI) {
+		angle += 2 * M_PI;
+	}
+	return angle ;
+};
+
+float Vec2::distanceVec2(const Vec2& other)
+{
+	Vec2 diff = diffVec2(other);
+	return diff.normVec2();
+};
--- practica1/src/main.cpp
+++ practica1/src/main.cpp
@@ -4,13 +4,93 @@
 #include <litegfx.h>
 #include <glfw3.h>
 #include <iostream>
+#include <Vec2.h>
+#include <cmath>
+#include <math.h>
 
 using namespace std;
 
+float rpX;
+float rpY;
+float npX;
+float npY;
+
 int main() {
+
 	
+
+	if (glfwInit())
+	{
+		double m_xpos;
+		double m_ypos; 
+		double* pm_xpos = &m_xpos;
+		double* pm_ypos = &m_ypos;
+		int w_width = 800;
+		int w_height = 600;
+		float cube_width = 50;
+		float cube_height = 50;
+
+		GLFWwindow* window = glfwCreateWindow(w_width, w_height, "OpenGL go brrrrrr", nullptr, nullptr);
+		glfwMakeContextCurrent(window);
+		lgfx_setup2d(w_width, w_height);
+		
+
+			//nuevaPosicionX = puntoDeRotacion.X + distance * Math.sin(angle);
+			//nuevaPosicionY = puntoDeRotacion.Y + distance * Math.cos(angle);
+		
+
+		do  {
+
+			float angle = (3.14f / 6) * glfwGetTime(); //el ángulo que va aumentando con respecto del tiempo
+
+			float rpX = (float)*pm_xpos - cube_width / 2; // posición X de referencia (del cubo)
+			float rpY = (float)*pm_ypos - cube_height / 2; // poisición Y de referencia (del cubo)
+			
+			Vec2 vecA = Vec2(rpX,rpY);
+			Vec2 vecB = Vec2(npX, npY);
+
+			/*float angle = vecA.angleVec2(vecB);
+			angle += 0, 55.f;*/
+
+			float npX = rpX + 100.f * sin(angle);  // *sin(vecA.angleVec2(vecB));
+			float npY = rpY + 100.f * cos(angle); // *cos(vecA.angleVec2(vecB));
+			
+			// pintar background
+			lgfx_clearcolorbuffer(0.3f, 0.3f, 0.3f);
+
+			//para obtener la posición del ratón
+			glfwGetCursorPos(window, pm_xpos, pm_ypos);
+
+			//pintar color cuadrado 1
+			lgfx_setcolor(0.1f, 0.1f, 0.1f, 1.f);
+
+			lgfx_drawrect(w_width/2 - cube_width/2, w_height/2 - cube_height/2, (float) cube_width, (float) cube_height);
+
+			lgfx_setcolor(1.f, 0.2f, 0.2f, 1.f);
+
+			//cuadrado que se mueve
+			lgfx_drawrect(rpX, rpY, (float)cube_width, (float)cube_height);
+
+			lgfx_setcolor(0.f, 1.2f, 0.2f, 1.f);
+
+			//círculo que orbita
+			lgfx_drawoval(npX, npY, 30.f, 30.f);
+			
+			glfwSwapBuffers(window);
+
+			//check and call events
+			glfwPollEvents();
+			
+		} while (!glfwWindowShouldClose(window));
 
 		
 
-    return 0;
+	}
+	else
+	{
+		printf("fallo");
+	}
+
+	glfwTerminate();
+	return 0;
 }
